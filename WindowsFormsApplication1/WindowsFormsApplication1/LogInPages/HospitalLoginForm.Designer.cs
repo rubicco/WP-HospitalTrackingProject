6fd059da6de8422c4a3176336ff38c080ec22c92
Miraç Polat <polatmirac@gmail.com>
2016-12-09 22:08:33 +0300

    deneme pull

--- WindowsFormsApplication1/WindowsFormsApplication1/LogInPages/HospitalLoginForm.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/LogInPages/HospitalLoginForm.Designer.cs
@@ -28,21 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.kullaniciAdiTextBox = new System.Windows.Forms.TextBox();
+            this.sifreTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.girisButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // kullaniciAdiTextBox
+            // 
+            this.kullaniciAdiTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kullaniciAdiTextBox.Location = new System.Drawing.Point(145, 54);
+            this.kullaniciAdiTextBox.Name = "kullaniciAdiTextBox";
+            this.kullaniciAdiTextBox.Size = new System.Drawing.Size(100, 20);
+            this.kullaniciAdiTextBox.TabIndex = 0;
+            // 
+            // sifreTextBox
+            // 
+            this.sifreTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sifreTextBox.Location = new System.Drawing.Point(145, 107);
+            this.sifreTextBox.Name = "sifreTextBox";
+            this.sifreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.sifreTextBox.TabIndex = 1;
+            this.sifreTextBox.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre:";
+            // 
+            // girisButton
+            // 
+            this.girisButton.FlatAppearance.BorderSize = 3;
+            this.girisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.girisButton.Location = new System.Drawing.Point(145, 155);
+            this.girisButton.Name = "girisButton";
+            this.girisButton.Size = new System.Drawing.Size(100, 33);
+            this.girisButton.TabIndex = 4;
+            this.girisButton.Text = "GİRİŞ";
+            this.girisButton.UseVisualStyleBackColor = true;
             // 
             // HospitalLoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(343, 232);
+            this.Controls.Add(this.girisButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sifreTextBox);
+            this.Controls.Add(this.kullaniciAdiTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "HospitalLoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "HospitalLoginForm";
+            this.Text = "Hastane Girişi";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.HospitalLoginForm_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox kullaniciAdiTextBox;
+        private System.Windows.Forms.TextBox sifreTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button girisButton;
     }
 }
