e983481536c56620139ae601947f29309dcbf3dc
mimkemal <mimkemaltekin@yahoo.com.tr>
2016-12-09 22:44:58 +0300

    deneme pull mkt

--- WindowsFormsApplication1/WindowsFormsApplication1/LogInPages/HospitalLoginForm.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/LogInPages/HospitalLoginForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.kullaniciAdiTextBox = new System.Windows.Forms.TextBox();
-            this.sifreTextBox = new System.Windows.Forms.TextBox();
+            this.parolaTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.girisButton = new System.Windows.Forms.Button();
@@ -38,60 +38,66 @@
             // kullaniciAdiTextBox
             // 
             this.kullaniciAdiTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.kullaniciAdiTextBox.Location = new System.Drawing.Point(145, 54);
+            this.kullaniciAdiTextBox.Location = new System.Drawing.Point(160, 50);
+            this.kullaniciAdiTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.kullaniciAdiTextBox.Name = "kullaniciAdiTextBox";
-            this.kullaniciAdiTextBox.Size = new System.Drawing.Size(100, 20);
+            this.kullaniciAdiTextBox.Size = new System.Drawing.Size(133, 22);
             this.kullaniciAdiTextBox.TabIndex = 0;
             // 
-            // sifreTextBox
+            // parolaTextBox
             // 
-            this.sifreTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.sifreTextBox.Location = new System.Drawing.Point(145, 107);
-            this.sifreTextBox.Name = "sifreTextBox";
-            this.sifreTextBox.Size = new System.Drawing.Size(100, 20);
-            this.sifreTextBox.TabIndex = 1;
-            this.sifreTextBox.UseSystemPasswordChar = true;
+            this.parolaTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.parolaTextBox.Location = new System.Drawing.Point(160, 116);
+            this.parolaTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.parolaTextBox.Name = "parolaTextBox";
+            this.parolaTextBox.Size = new System.Drawing.Size(133, 22);
+            this.parolaTextBox.TabIndex = 1;
+            this.parolaTextBox.UseSystemPasswordChar = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 57);
+            this.label1.Location = new System.Drawing.Point(58, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(88, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Kullanıcı Adı:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(104, 110);
+            this.label2.Location = new System.Drawing.Point(106, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.Size = new System.Drawing.Size(53, 17);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Şifre:";
+            this.label2.Text = "Parola:";
             // 
             // girisButton
             // 
             this.girisButton.FlatAppearance.BorderSize = 3;
             this.girisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.girisButton.Location = new System.Drawing.Point(145, 155);
+            this.girisButton.Location = new System.Drawing.Point(160, 175);
+            this.girisButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.girisButton.Name = "girisButton";
-            this.girisButton.Size = new System.Drawing.Size(100, 33);
+            this.girisButton.Size = new System.Drawing.Size(133, 41);
             this.girisButton.TabIndex = 4;
             this.girisButton.Text = "GİRİŞ";
             this.girisButton.UseVisualStyleBackColor = true;
+            this.girisButton.Click += new System.EventHandler(this.girisButton_Click);
             // 
             // HospitalLoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 232);
+            this.ClientSize = new System.Drawing.Size(368, 287);
             this.Controls.Add(this.girisButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.sifreTextBox);
+            this.Controls.Add(this.parolaTextBox);
             this.Controls.Add(this.kullaniciAdiTextBox);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "HospitalLoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hastane Girişi";
@@ -104,7 +110,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox kullaniciAdiTextBox;
-        private System.Windows.Forms.TextBox sifreTextBox;
+        private System.Windows.Forms.TextBox parolaTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button girisButton;
